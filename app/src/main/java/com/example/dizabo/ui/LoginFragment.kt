02e9f5413fa15625428f53cdde9b505d43c3f631
Suhail <suhailabbas23@gmail.com>
2023-07-18 18:17:32 +0530

    Added Navigation component

---
+++ app/src/main/java/com/example/dizabo/ui/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.dizabo.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dizabo.R
+import com.example.dizabo.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
